Correct argument and remove redundant softmax operation.

convnext_tf/convnext_v2.py
@@ -24,7 +24,7 @@ class GRN(layers.Layer):
         )
 
     def call(self, x):
-        Gx = tf.norm(x, ord=2, axis=(1,2), keepdims=True)
+        Gx = tf.norm(x, ord='fro', axis=(1,2), keepdims=True)
         Nx = Gx / (tf.math.reduce_mean(Gx, axis=-1, keepdims=True) + 1e-6)
         return self.gamma * (x * Nx) + self.beta + x
 